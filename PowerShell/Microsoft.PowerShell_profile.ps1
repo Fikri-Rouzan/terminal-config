ef2b2631bbc301dae70308c2f3980a5d083cc70b
Fikri-Rouzan <fikrirzn@gmail.com>
2025-12-14 09:26:55 +0700

    first commit

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+oh-my-posh init pwsh --config "C:\Users\%USERPROFILE%\Documents\PowerShell\Themes\atomic.omp.json" | Invoke-Expression
+
+Import-Module Terminal-Icons
+Import-Module z
+Import-Module PSReadLine
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+$host.ui.RawUI.WindowTitle = "{{ .HostName }}"
+clear -x
+fastfetch
